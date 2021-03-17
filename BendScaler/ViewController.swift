971e80d6930b87237833fcf1927857e037c3eb02
Ataberk <ataberkitu@gmail.com>
2021-03-17 20:50:12 +0300

    v0.1

--- BendScaler/ViewController.swift
+++ BendScaler/ViewController.swift
@@ -6,14 +6,210 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import Layoutless
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var steps: [MKRoute.Step] = []
+    var stepCounter = 0
+    var route: MKRoute?
+    var showMapRoute = false
+    var navigationStarted = false
+    let locationDistance: Double = 1000
+    
+    var speecher = AVSpeechSynthesizer()
+    
+    lazy var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        let status = locationManager.authorizationStatus
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            handleAuthorizationStatus(locationManager: locationManager, status: status)
+        }
+        else{
+            print("Enable Location Services")
+        }
+        return locationManager
+    }()
+    
+    lazy var directionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Where do you want to go?"
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var textField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter your destination"
+        tf.borderStyle = .roundedRect
+        return tf
+    }()
+    
+    lazy var getDirectionButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Get Direction", for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
+        button.addTarget(self, action: #selector(getDirectionButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var startStopButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Start", for: .normal)
+        button.setTitleColor(.systemRed, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 48)
+        button.addTarget(self, action: #selector(startStopButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        return mapView
+    }()
+    
+    @objc fileprivate func getDirectionButtonTapped() {
+        guard let text = textField.text
+        else{
+            return
+        }
+        showMapRoute = true
+        textField.endEditing(true)
+        
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(text){ (placemarks, err) in
+            if let err = err{
+                print(err.localizedDescription)
+                return
+            }
+            guard let placemarks = placemarks,
+                  let placemark = placemarks.first,
+                  let location = placemark.location
+            else{return}
+            let destinationCoordinate = location.coordinate
+            self.mapRoute(destinationCoordinate: destinationCoordinate)
+        }
+    }
+    
+    @objc fileprivate func startStopButtonTapped() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
+        locationManager.startUpdatingLocation()
     }
 
+    fileprivate func setupViews() {
+        view.backgroundColor = .systemBackground
+        
+        stack(.vertical)(
+            directionLabel.insetting(by: 16),
+            stack(.horizontal, spacing: 16)(
+                textField,
+                getDirectionButton
+            ).insetting(by: 16),
+            startStopButton.insetting(by: 16),
+            mapView
+        ).fillingParent(relativeToSafeArea: true).layout(in: view)
+    }
+    
+    fileprivate func centerViewToUserLocation(center: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: locationDistance, longitudinalMeters: locationDistance)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    fileprivate func handleAuthorizationStatus(locationManager: CLLocationManager, status: CLAuthorizationStatus) {
+        switch status{
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break
+        case .restricted:
+            //
+            break
+        case .denied:
+            //
+            break
+        case .authorizedAlways:
+            //
+            break
+        case .authorizedWhenInUse:
+            if let center = locationManager.location?.coordinate{
+                centerViewToUserLocation(center: center)
+            }
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    fileprivate func mapRoute(destinationCoordinate: CLLocationCoordinate2D) {
+        guard let sourceCoordinate = locationManager.location?.coordinate else{return}
+        let sourcePlacemark = MKPlacemark(coordinate: sourceCoordinate)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationItem = MKMapItem(placemark: destinationPlacemark)
+        
+        let routeRequest = MKDirections.Request()
+        routeRequest.source = sourceItem
+        routeRequest.destination = destinationItem
+        routeRequest.transportType = .automobile
+        
+        let directions = MKDirections(request: routeRequest)
+        directions.calculate{ (response, err) in
+            if let err = err{
+                print(err.localizedDescription)
+                return
+            }
+            guard let response = response,let route = response.routes.first
+            else{return}
+            
+            self.route = route
+            self.mapView.addOverlay(route.polyline)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, edgePadding: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16), animated: true)
+            self.getRouteSteps(route: route)
+        }
+    }
+    
+    fileprivate func getRouteSteps(route: MKRoute) {
+        
+    }
 
+}
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !showMapRoute {
+            if let location = locations.last{
+                let center = location.coordinate
+                centerViewToUserLocation(center: center)
+            }
+        }
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        handleAuthorizationStatus(locationManager: locationManager, status: locationManager.authorizationStatus)
+    }
+}
+
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .systemPurple
+        return renderer
+    }
 }
 
